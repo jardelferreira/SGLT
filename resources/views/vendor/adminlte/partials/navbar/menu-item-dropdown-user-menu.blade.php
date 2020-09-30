8a8376b0c0369eb15a83c0036c17f3e27851bd88
jardelferreira <jardelferreira.sousa@gmail.com>
2020-09-29 22:33:27 -0300

    publish adminLte views

---
+++ resources/views/vendor/adminlte/partials/navbar/menu-item-dropdown-user-menu.blade.php
@@ -0,0 +1,138 @@
+@php( $logout_url = View::getSection('logout_url') ?? config('adminlte.logout_url', 'logout') )
+@php( $profile_url = View::getSection('profile_url') ?? config('adminlte.profile_url', 'logout') )
+
+@if (config('adminlte.usermenu_profile_url', false))
+    @php( $profile_url = Auth::user()->adminlte_profile_url() )
+@endif
+
+@if (config('adminlte.use_route_url', false))
+    @php( $profile_url = $profile_url ? route($profile_url) : '' )
+    @php( $logout_url = $logout_url ? route($logout_url) : '' )
+@else
+    @php( $profile_url = $profile_url ? url($profile_url) : '' )
+    @php( $logout_url = $logout_url ? url($logout_url) : '' )
+@endif
+
+<li class="nav-item dropdown user-menu">
+
+    {{-- User menu toggler --}}
+    <a href="#" class="nav-link dropdown-toggle d-flex" data-toggle="dropdown">
+        @if(config('adminlte.usermenu_image'))
+            <img src="{{ Auth::user()->adminlte_image() }}"
+                 class="user-image img-circle elevation-2"
+                 alt="{{ Auth::user()->name }}">
+        @endif
+        <span @if(config('adminlte.usermenu_image')) class="d-none d-md-inline" @endif>
+            {{ Auth::user()->name }}
+        </span>
+    </a>
+
+    {{-- User menu dropdown --}}
+    <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+
+        {{-- User menu header --}}
+        @if(!View::hasSection('usermenu_header') && config('adminlte.usermenu_header'))
+            <li class="user-header {{ config('adminlte.usermenu_header_class', 'bg-primary') }}
+                @if(!config('adminlte.usermenu_image')) h-auto @endif">
+                @if(config('adminlte.usermenu_image'))
+                    <img src="{{ Auth::user()->adminlte_image() }}"
+                         class="img-circle elevation-2"
+                         alt="{{ Auth::user()->name }}">
+                @endif
+                <p class="@if(!config('adminlte.usermenu_image')) mt-0 @endif">
+                    {{ Auth::user()->name }}
+                    @if(config('adminlte.usermenu_desc'))
+                        <small>{{ Auth::user()->adminlte_desc() }}</small>
+                    @endif
+                </p>
+            </li>
+        @else
+            @yield('usermenu_header')
+        @endif
+
+        {{-- Configured user menu links --}}
+        @each('adminlte::partials.navbar.dropdown-item', $adminlte->menu("navbar-user"), 'item')
+
+        {{-- User menu body --}}
+        @hasSection('usermenu_body')
+            <li class="user-body">
+                @yield('usermenu_body')
+            </li>
+        @endif
+
+        {{-- User menu footer --}}
+        <li class="user-footer">
+            @if($profile_url)
+                <a href="{{ $profile_url }}" class="btn btn-default btn-flat">
+                    <i class="fa fa-fw fa-user"></i>
+                    {{ __('adminlte::menu.profile') }}
+                </a>
+            @endif
+            <div class="mt-3 space-y-1">
+                <!-- Account Management -->
+                <x-jet-responsive-nav-link href="/user/profile" :active="request()->routeIs('profile.show')">
+                    {{ __('Profile') }}
+                </x-jet-responsive-nav-link>
+
+                @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
+                    <x-jet-responsive-nav-link href="/user/api-tokens" :active="request()->routeIs('api-tokens.index')">
+                        {{ __('API Tokens') }}
+                    </x-jet-responsive-nav-link>
+                @endif
+
+                <!-- Authentication -->
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+
+                    <x-jet-responsive-nav-link href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                        {{ __('Logout') }}
+                    </x-jet-responsive-nav-link>
+                </form>
+
+                <!-- Team Management -->
+                @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
+                    <div class="border-t border-gray-200"></div>
+
+                    <div class="block px-4 py-2 text-xs text-gray-400">
+                        {{ __('Manage Team') }}
+                    </div>
+
+                    <!-- Team Settings -->
+                    <x-jet-responsive-nav-link href="/teams/{{ Auth::user()->currentTeam->id }}" :active="request()->routeIs('teams.show')">
+                        {{ __('Team Settings') }}
+                    </x-jet-responsive-nav-link>
+
+                    <x-jet-responsive-nav-link href="/teams/create" :active="request()->routeIs('teams.create')">
+                        {{ __('Create New Team') }}
+                    </x-jet-responsive-nav-link>
+
+                    <div class="border-t border-gray-200"></div>
+
+                    <!-- Team Switcher -->
+                    <div class="block px-4 py-2 text-xs text-gray-400">
+                        {{ __('Switch Teams') }}
+                    </div>
+
+                    @foreach (Auth::user()->allTeams() as $team)
+                        <x-jet-switchable-team :team="$team" component="jet-responsive-nav-link" />
+                    @endforeach
+                @endif
+            </div>
+            <a class="btn btn-default btn-flat float-right @if(!$profile_url) btn-block @endif"
+               href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                <i class="fa fa-fw fa-power-off"></i>
+                {{ __('adminlte::adminlte.log_out') }}
+            </a>
+            <form id="logout-form" action="{{ $logout_url }}" method="POST" style="display: none;">
+                @if(config('adminlte.logout_method'))
+                    {{ method_field(config('adminlte.logout_method')) }}
+                @endif
+                {{ csrf_field() }}
+            </form>
+        </li>
+
+    </ul>
+
+</li>
