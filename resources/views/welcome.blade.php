cd420f9910297ef636712107373229c31b8522e8
jardelferreira <jardelferreira.sousa@gmail.com>
2020-10-04 15:44:25 -0300

    title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/home.css')}}">
     <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
-    <title>Hello, world!</title>
+    <title>Home page</title>
   </head>
   <body>
     <body id="page-top">
